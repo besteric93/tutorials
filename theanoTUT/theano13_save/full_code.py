2737579c27782171ea99996fdea493766a81e86b
Eric <ericbarzelogna@gmail.com>
2020-06-07 12:56:35 -0400

    fullcode

--- theanoTUT/theano13_save/full_code.py
+++ theanoTUT/theano13_save/full_code.py
@@ -5,7 +5,7 @@
 
 # 13 - save and reload
 """
-Please note, this code is only for python 3+. If you are using python 2+, please modify the code accordingly.
+this code is only for python 3+. If you are using python 2+, please modify the code accordingly.
 """
 from __future__ import print_function
 import numpy as np
